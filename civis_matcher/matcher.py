500f8501663bb47ac108e77c4491607ebf208556
Mark Rogers <f4nt@f4ntasmic.com>
2013-07-18 13:32:13 -0500

    bulk matching support in the wrapper

--- civis_matcher/matcher.py
+++ civis_matcher/matcher.py
@@ -34,6 +34,7 @@ class MatchResult(Struct):
 
     def __init__(self, **entries):
         self.__dict__.update(entries)
+        self.url = ''
         people_list = []
         if self.people:
             for person in self.people:
@@ -54,7 +55,7 @@ class MatchResult(Struct):
 class CivisMatcher(object):
 
     def __init__(self, user='edgeflip', password='civis!19',
-                cache_hosts=[], cache_expiry=3600):
+                cache_hosts=[], cache_expiry=3600, base_url=''):
         self.auth = (user, password)
         self.caching_enabled = False
         self.expiry = cache_expiry
@@ -62,29 +63,28 @@ class CivisMatcher(object):
             self.cache = pylibmc.Client(cache_hosts)
             self.caching_enabled = True
 
-    def _check_cache(self, url):
+        # Useful if you want to test against their staging instance
+        self.base_url = base_url if base_url else CIVIS_BASE_URL
+
+    def _check_cache(self, url, params):
         ''' Checks the cache for Civis match results. Hashes the URL plus the
         params used in the check to create a key.
         '''
         if not self.caching_enabled:
             return None
 
-        url_hash = hashlib.md5(url).hexdigest()
+        url_hash = hashlib.md5('%s?%s' % (url, params)).hexdigest()
         return self.cache.get(url_hash)
 
-    def _set_cache(self, url, data):
+    def _set_cache(self, url, params, data):
         ''' Sets the cache with the key being a hashed form of the URL and
         params
         '''
         if self.caching_enabled:
-            url_hash = hashlib.md5(url).hexdigest()
+            url_hash = hashlib.md5('%s?%s' % (url, params)).hexdigest()
             self.cache.set(url_hash, data, time=self.expiry)
 
-    def _check_civis(self, url):
-        ''' Makes an actual call to Civis in the event that we don't already
-        have anything stored in the cache
-        '''
-        resp = requests.get(url, auth=self.auth, timeout=5)
+    def _validate_result(self, resp):
         if resp.status_code != 200:
             raise MatchException(
                 'Invalid response code: %s, url: %s' % (
@@ -94,7 +94,7 @@ class CivisMatcher(object):
             )
 
         data = json.loads(resp.content)
-        if data['error']:
+        if data.get('error'):
             raise MatchException(
                 'Error returned by Civis: id: %s, message: %s, url: %s' % (
                     data['error_id'],
@@ -102,19 +102,39 @@ class CivisMatcher(object):
                     resp.url
                 )
             )
-
-        self._set_cache(url, data)
         return data
 
-    def _make_request(self, url, params):
+    def _get(self, url, params):
+        resp = requests.get(url, auth=self.auth, timeout=5)
+        data = self._validate_result(resp)
+        self._set_cache(url, params, data)
+        return data
+
+    def _post(self, url, params):
+        resp = requests.post(url, data=json.dumps(params),
+                            auth=self.auth, timeout=5)
+        data = self._validate_result(resp)
+        post_url = '%s?%s' % (url, urlencode(params))
+        self._set_cache(post_url, params, data)
+        return data
+
+    def _check_civis(self, url, params, method):
+        ''' Makes an actual call to Civis in the event that we don't already
+        have anything stored in the cache
+        '''
+        if method == 'GET':
+            return self._get(url, params)
+        elif method == 'POST':
+            return self._post(url, params)
+
+    def _make_request(self, url, params, method='GET'):
         ''' Helper function for making a request to Civis. Checks our cache
         before moving on the actually make a request to Civis.
         '''
-        req_url = '%s?%s' % (url, urlencode(params))
-        data = self._check_cache(req_url)
+        data = self._check_cache(url, params)
         if not data:
-            data = self._check_civis(req_url)
-        return data, req_url
+            data = self._check_civis(url, params, method)
+        return data, '%s?%s' % (url, urlencode(params))
 
     def match(self, first_name, last_name, **kwargs):
         '''
@@ -129,7 +149,7 @@ class CivisMatcher(object):
             city
 
         '''
-        url = '%s/match' % CIVIS_BASE_URL
+        url = '%s/match' % self.base_url
         request_params = kwargs
         request_params.update({
             'first_name': first_name,
@@ -139,3 +159,66 @@ class CivisMatcher(object):
 
         data['result'].update({'url': req_url})
         return MatchResult(**data['result'])
+
+    def bulk_match(self, match_dict):
+        '''
+        Performs a bulk match query against the Civis Matching service. Expects
+        a dictionary structured like so:
+
+            {'people': {
+                '01': {
+                    'first_name': 'Test',
+                    'last_name': 'User1',
+                    'state': 'IL',
+                    'city': 'Chicago',
+                },
+                '02': {
+                    'first_name': 'Test',
+                    'last_name': 'User2',
+                    'state': 'IL',
+                    'city': 'Chicago',
+                }
+            }
+
+        Responses look like:
+
+            {u'0': {u'error': False,
+                u'result': {u'more_people': False,
+                    u'people': [{u'TokenCount': 6,
+                        u'birth_day': u'01',
+                        u'birth_month': u'12',
+                        u'birth_year': u'1969',
+                        u'city': u'CHARLOTTESVILLE',
+                        u'dma': u'584',
+                        u'dma_name': u'Charlottesville VA',
+                        u'first_name': u'TEST',
+                        u'gender': u'M',
+                        u'id': u'16595385',
+                        u'last_name': u'USER',
+                        u'nick_name': u'TESTUSER',
+                        u'scores': {u'gotv_score': 0,
+                        u'persuasion_score': 25.59,
+                        u'persuasion_score_dec': 3,
+                        u'support_cand_2013': 52.085,
+                        u'support_cand_2013_dec': 7,
+                        u'turnout_2013': 85.419,
+                        u'turnout_2013_dec': 9},
+                        u'state': u'VA'}],
+                    u'people_count': 1,
+                    u'scores': {u'gotv_score': {u'count': 1,
+                        u'max': 0,
+                        u'mean': 0,
+                        u'min': 0,
+                        u'std': 0}
+                    }
+                }
+            }
+        }
+
+        '''
+        url = '%s/multimatch' % self.base_url
+        data, req_url = self._make_request(url, match_dict, 'POST')
+        full_result = {}
+        for k, v in data.items():
+            full_result[k] = MatchResult(**v['result'])
+        return full_result
--- civis_matcher/tests.py
+++ civis_matcher/tests.py
@@ -12,8 +12,10 @@ class TestCivisMatcher(unittest.TestCase):
 
         # Requests Mock
         self.orig_requests_get = matcher.requests.get
+        self.orig_requests_post = matcher.requests.post
         self.requests_mock = Mock()
         matcher.requests.get = self.requests_mock
+        matcher.requests.post = self.requests_mock
 
         # pylibmc Mock
         self.orig_cache_client = matcher.pylibmc.Client
@@ -28,6 +30,7 @@ class TestCivisMatcher(unittest.TestCase):
 
     def tearDown(self):
         matcher.requests.get = self.orig_requests_get
+        matcher.requests.post = self.orig_requests_post
         matcher.pylibmc.Client = self.orig_cache_client
 
         super(TestCivisMatcher, self).tearDown()
@@ -110,3 +113,69 @@ class TestCivisMatcher(unittest.TestCase):
         self.assertEqual(person.last_name, 'User')
         self.assertEqual(result.score_mean, 357)
         self.assertEqual(result.people_count, 1)
+
+    def test_successful_bulk_match(self):
+        ''' Tests a successful match against the multimatch end point '''
+        self. requests_mock.return_value = Mock(
+            status_code=200,
+            url='http://example.com/test-failure',
+            content=json.dumps({
+                u'0': {
+                    u'error': False,
+                    u'result': {u'more_people': False,
+                    u'people': [{u'TokenCount': 6,
+                            u'birth_day': u'01',
+                            u'birth_month': u'12',
+                            u'birth_year': u'1969',
+                            u'city': u'CHARLOTTESVILLE',
+                            u'dma': u'584',
+                            u'dma_name': u'Charlottesville VA',
+                            u'first_name': u'TEST',
+                            u'gender': u'M',
+                            u'id': u'16595385',
+                            u'last_name': u'USER',
+                            u'nick_name': u'TESTUSER',
+                            u'scores': {
+                                u'gotv_score': 0,
+                                u'persuasion_score': 25.59,
+                                u'persuasion_score_dec': 3,
+                                u'support_cand_2013': 52.085,
+                                u'support_cand_2013_dec': 7,
+                                u'turnout_2013': 85.419,
+                                u'turnout_2013_dec': 9},
+                            u'state': u'VA'}],
+                    u'people_count': 1,
+                    }
+                }
+            })
+        )
+        match_dict = {
+            'people': {
+                0: {'city': 'Arlington',
+                    'first_name': 'Molly',
+                    'last_name': 'Ball',
+                    'state': 'VA'},
+                1: {'city': 'Burke',
+                    'first_name': 'Luanne',
+                    'last_name': 'Smith',
+                    'state': 'VA'},
+                2: {'birth_day': '04',
+                    'birth_month': '05',
+                    'birth_year': '1958',
+                    'city': 'Chester',
+                    'first_name': 'David',
+                    'last_name': 'Martin',
+                    'state': 'VA'},
+                3: {'birth_day': '01',
+                    'birth_month': '12',
+                    'birth_year': '1969',
+                    'city': 'Charlottesville',
+                    'first_name': 'David',
+                    'last_name': 'Swanson',
+                    'state': 'VA'}
+            }
+        }
+        result = self.cm.bulk_match(match_dict)['0']
+        self.assertEqual(result.people[0].first_name, 'TEST')
+        self.assertEqual(result.people[0].last_name, 'USER')
+        self.assertEqual(result.people[0].scores['persuasion_score'], 25.59)
